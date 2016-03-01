c500b254bfddcfc1959ec84a6c9317bd9b970c18
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-03-01 16:24:44 +0800

    the game page is accessible now

--- features/step_definitions/hangman_steps.rb
+++ features/step_definitions/hangman_steps.rb
@@ -1,9 +1,8 @@
 Given(/^a word "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
 end
 
 When(/^game start$/) do
-  pending # express the regexp above with the code you wish you had
+	visit "http://localhost:3000/game/index"
 end
 
 Then(/^tries is (\d+)$/) do |arg1|
