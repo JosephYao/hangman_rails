834633b1f3522ad8d870d94e4925cb255689ea92
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-04-14 17:43:56 +0800

    refactor controller test

--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -7,30 +7,34 @@ RSpec.describe GameController, type: :controller do
 		let(:word_to_guess) { 'word' }
 		let(:length_of_word) { word_to_guess.length }
 		let(:max_tries) { 12 }
+		let(:hangman) { stub_hangman }
 
 		it "response length of word and tries to view" do
-			hangman = double()
-			allow(Hangman).to receive(:new).and_return(hangman)
 			allow(hangman).to receive(:length).and_return(length_of_word)
 			allow(hangman).to receive(:tries).and_return(max_tries)
-			allow(hangman).to receive(:used).and_return('used')
 
-			get :index, {word: word_to_guess}
+			start_game
 
 			expect(assigns(:length)).to eq(length_of_word)
 			expect(assigns(:tries)).to eq(max_tries)
 		end
 	  
 		it "response used to view in upper case" do
-			hangman = double()
-			allow(Hangman).to receive(:new).and_return(hangman)
-			allow(hangman).to receive(:length).and_return(length_of_word)
-			allow(hangman).to receive(:tries).and_return(max_tries)
 			allow(hangman).to receive(:used).and_return('used')
 
-			get :index, {word: word_to_guess}
+			start_game
 
 			expect(assigns(:used)).to eq('USED')
+		end
+
+		def start_game
+			get :index, {word: word_to_guess}
+		end
+
+		def stub_hangman
+			hangman = Hangman.new '' 
+			allow(Hangman).to receive(:new).and_return(hangman)
+			return hangman
 		end
 	end
 
